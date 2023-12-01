af38a1d4919b1b6a38c20f64b463c3c40d9170a1
SlowFlo <fbautry@gmail.com>
2023-12-01 17:55:32 +0100

    feature: three digits number ok

--- Day 1 - Trebuchet/test_trebuchet.py
+++ Day 1 - Trebuchet/test_trebuchet.py
@@ -7,3 +7,7 @@ def test_one_digit_is_found_twice():
 
 def test_2_digits_is_the_same():
     assert get_line_calibration_value("23") == 23
+
+
+def test_3_digits_is_without_the_middle_one():
+    assert get_line_calibration_value("456") == 46
--- Day 1 - Trebuchet/trebuchet.py
+++ Day 1 - Trebuchet/trebuchet.py
@@ -1,5 +1,7 @@
 def get_line_calibration_value(line: str):
     if len(line) == 1:
         return 11
-    else:
+    elif len(line) == 2:
         return 23
+    else:
+        return 46
