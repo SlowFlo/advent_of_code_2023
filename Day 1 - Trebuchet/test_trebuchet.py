25621720c3df613e1d7bd3d4040c06845fc74098
SlowFlo <fbautry@gmail.com>
2023-12-01 18:14:31 +0100

    feature: works with alphabet characters on the sides

--- Day 1 - Trebuchet/test_trebuchet.py
+++ Day 1 - Trebuchet/test_trebuchet.py
@@ -17,3 +17,7 @@ def test_3_digits_is_without_the_middle_one():
 
 def test_has_letters_between_is_ok():
     assert get_line_calibration_value("1abc2") == 12
+
+
+def test_has_letters_on_sides_is_ok():
+    assert get_line_calibration_value("pqr3stu8vwx") == 38
--- Day 1 - Trebuchet/trebuchet.py
+++ Day 1 - Trebuchet/trebuchet.py
@@ -1,4 +1,5 @@
 def get_line_calibration_value(line: str):
+    line = "".join(filter(str.isdigit, line))
     if len(line) == 1:
         return int(line + line)
     elif len(line) == 2:
