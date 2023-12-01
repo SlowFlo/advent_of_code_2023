00a81ae7f1314f964e2ee24e86c8098346744c0d
SlowFlo <fbautry@gmail.com>
2023-12-01 18:29:55 +0100

    feature: add main

--- Day 1 - Trebuchet/trebuchet.py
+++ Day 1 - Trebuchet/trebuchet.py
@@ -8,3 +8,12 @@ def get_line_calibration_value(line: str):
         return int(line + line)
     else:
         return int(line[0] + line[-1])
+
+
+if __name__ == "__main__":
+    with open("input.txt", "r") as file:
+        input_text = file.read()
+        print(
+            "The sum of all calibration values are:",
+            get_sum_calibration_values(input_text),
+        )
