a15006b64c1f9cb1e5cf7f15285c09aa4c422cf0
SlowFlo <fbautry@gmail.com>
2023-12-01 17:48:17 +0100

    feature: first test pass

--- Day 1 - Trebuchet/test_trebuchet.py
+++ Day 1 - Trebuchet/test_trebuchet.py
@@ -1,2 +1,5 @@
-def assert_one_digit_is_found_twice():
-    assert get_line_calibration_value("1") == 1
+from trebuchet import get_line_calibration_value
+
+
+def test_one_digit_is_found_twice():
+    assert get_line_calibration_value("1") == 11
---
+++ Day 1 - Trebuchet/trebuchet.py
@@ -0,0 +1,2 @@
+def get_line_calibration_value(line: str):
+    return 11
