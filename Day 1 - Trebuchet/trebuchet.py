b823e933b14992b47232664157fb90f18ad0a457
SlowFlo <fbautry@gmail.com>
2023-12-01 18:15:32 +0100

    refactor: simplification

--- Day 1 - Trebuchet/trebuchet.py
+++ Day 1 - Trebuchet/trebuchet.py
@@ -2,7 +2,5 @@ def get_line_calibration_value(line: str):
     line = "".join(filter(str.isdigit, line))
     if len(line) == 1:
         return int(line + line)
-    elif len(line) == 2:
-        return int(line)
     else:
         return int(line[0] + line[-1])
