19396f585a2f1949352ffa39e26b88814cd7404c
SlowFlo <fbautry@gmail.com>
2023-12-01 18:04:02 +0100

    test: add a test but already satisfied

--- Day 1 - Trebuchet/test_trebuchet.py
+++ Day 1 - Trebuchet/test_trebuchet.py
@@ -3,6 +3,7 @@ from trebuchet import get_line_calibration_value
 
 def test_one_digit_is_found_twice():
     assert get_line_calibration_value("1") == 11
+    assert get_line_calibration_value("2") == 22
 
 
 def test_2_digits_is_the_same():
@@ -12,3 +13,7 @@ def test_2_digits_is_the_same():
 def test_3_digits_is_without_the_middle_one():
     assert get_line_calibration_value("456") == 46
     assert get_line_calibration_value("789") == 79
+
+
+def test_has_letters_between_is_ok():
+    assert get_line_calibration_value("1abc2") == 12
