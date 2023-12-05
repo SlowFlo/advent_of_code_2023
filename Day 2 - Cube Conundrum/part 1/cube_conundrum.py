6b9f5f7cb36d648bb0837ca5ffed8eb07d24c4ab
SlowFlo <fbautry@gmail.com>
2023-12-05 13:51:23 +0100

    feature: detection of maximum for each colors' games

--- Day 2 - Cube Conundrum/part 1/cube_conundrum.py
+++ Day 2 - Cube Conundrum/part 1/cube_conundrum.py
@@ -1,5 +1,16 @@
+import re
 from typing import Dict
 
 
 def get_game_colors_maximum(game: str) -> Dict[str, int]:
-    return {"green": 2, "blue": 6, "red": 4}
+    colors_count = re.split(", |; ", game)
+
+    colors_dict = {"red": 0, "green": 0, "blue": 0}
+
+    for color_count in colors_count:
+        count, color = color_count.split()
+        count = int(count)
+        if colors_dict[color] < count:
+            colors_dict[color] = count
+
+    return colors_dict
