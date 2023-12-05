ed02c14af1f7d2453d3595b3e682d63e819bd508
SlowFlo <fbautry@gmail.com>
2023-12-05 15:54:19 +0100

    feature: is_game_possible

--- Day 2 - Cube Conundrum/part 1/cube_conundrum.py
+++ Day 2 - Cube Conundrum/part 1/cube_conundrum.py
@@ -2,7 +2,7 @@ import re
 from typing import Dict
 
 
-def get_game_colors_maximum(game: str) -> Dict[str, int]:
+def get_game_colors_minimum(game: str) -> Dict[str, int]:
     colors_count = re.split(", |; ", game)
 
     colors_dict = {"red": 0, "green": 0, "blue": 0}
@@ -14,3 +14,14 @@ def get_game_colors_maximum(game: str) -> Dict[str, int]:
             colors_dict[color] = count
 
     return colors_dict
+
+
+def is_game_possible(bag_content: Dict[str, int], game: str) -> bool:
+    colors_min_detected = get_game_colors_minimum(game)
+
+    game_is_possible = True
+    for color in bag_content:
+        if colors_min_detected[color] > bag_content[color]:
+            game_is_possible = False
+
+    return game_is_possible
--- Day 2 - Cube Conundrum/part 1/test_cube_conundrum.py
+++ Day 2 - Cube Conundrum/part 1/test_cube_conundrum.py
@@ -1,20 +1,20 @@
-from cube_conundrum import get_game_colors_maximum
+from cube_conundrum import get_game_colors_minimum, is_game_possible
 
 
 def test_get_each_color_maximum():
-    assert get_game_colors_maximum(
+    assert get_game_colors_minimum(
         "3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
     ) == {"blue": 6, "red": 4, "green": 2}
-    assert get_game_colors_maximum(
+    assert get_game_colors_minimum(
         "1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"
     ) == {"blue": 4, "red": 1, "green": 3}
-    assert get_game_colors_maximum(
+    assert get_game_colors_minimum(
         "8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"
     ) == {"blue": 6, "red": 20, "green": 13}
-    assert get_game_colors_maximum(
+    assert get_game_colors_minimum(
         "1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"
     ) == {"blue": 15, "red": 14, "green": 3}
-    assert get_game_colors_maximum(
+    assert get_game_colors_minimum(
         "6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
     ) == {"blue": 2, "red": 6, "green": 3}
 
