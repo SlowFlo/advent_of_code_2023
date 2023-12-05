a9cc82da5d4c047d9508c13714004fc19d7ca808
SlowFlo <fbautry@gmail.com>
2023-12-05 15:58:09 +0100

    test: test on the whole example as a multiple line input

--- Day 2 - Cube Conundrum/part 1/test_cube_conundrum.py
+++ Day 2 - Cube Conundrum/part 1/test_cube_conundrum.py
@@ -55,3 +55,13 @@ def test_can_tell_if_game_is_possible():
         )
         == True
     )
+
+
+def test_sum_of_possible_games_ids_is_ok():
+    text_file = """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"""
+
+    assert get_sum_of_possible_games_ids(text_file) == 8
