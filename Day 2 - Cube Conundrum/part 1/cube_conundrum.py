383dcb47e9d972a039b8b4460292b64229878e43
SlowFlo <fbautry@gmail.com>
2023-12-05 12:24:50 +0100

    feature: first test ok

---
+++ Day 2 - Cube Conundrum/part 1/cube_conundrum.py
@@ -0,0 +1,5 @@
+from typing import Dict
+
+
+def get_game_colors_maximum(game: str) -> Dict[str, int]:
+    return {"green": 2, "blue": 6, "red": 4}
--- Day 2 - Cube Conundrum/part 1/test_cube_conundrum.py
+++ Day 2 - Cube Conundrum/part 1/test_cube_conundrum.py
@@ -1,3 +1,6 @@
+from cube_conundrum import get_game_colors_maximum
+
+
 def test_get_each_color_maximum():
     assert get_game_colors_maximum(
         "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
