62be57f04929ad9fbf98a958ba90467f46ec63f0
SlowFlo <fbautry@gmail.com>
2023-12-10 17:51:56 +0100

    style: test expect values on one line

--- Day 4 - Scratchcards/part 1/test_scratchcards.py
+++ Day 4 - Scratchcards/part 1/test_scratchcards.py
@@ -4,21 +4,6 @@ from scratchcards import get_winning_and_i_have_numbers
 def test_get_winning_and_i_have_numbers():
     card = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
     assert get_winning_and_i_have_numbers(card) == (
-        (
-            41,
-            48,
-            83,
-            86,
-            17,
-        ),
-        (
-            83,
-            86,
-            6,
-            31,
-            17,
-            9,
-            48,
-            53,
-        ),
+        (41, 48, 83, 86, 17),
+        (83, 86, 6, 31, 17, 9, 48, 53),
     )
