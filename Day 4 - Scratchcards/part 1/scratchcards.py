a155700cbbb8c33b1306b95a7558400a1400931f
SlowFlo <fbautry@gmail.com>
2023-12-10 18:13:55 +0100

    feat: winning_numbers_i_have + convert to set

--- Day 4 - Scratchcards/part 1/scratchcards.py
+++ Day 4 - Scratchcards/part 1/scratchcards.py
@@ -5,9 +5,12 @@ class Scratchcard:
     def __init__(self, card: str):
         _, winning_numbers, numbers_i_have = re.split(r"[:|]", card)
 
-        self.winning_numbers = tuple(
+        self.winning_numbers = set(
             int(winning_number) for winning_number in winning_numbers.split()
         )
-        self.numbers_i_have = tuple(
+        self.numbers_i_have = set(
             int(numbers_i_have) for numbers_i_have in numbers_i_have.split()
+        )
+        self.winning_numbers_i_have = self.winning_numbers.intersection(
+            self.numbers_i_have
         )
--- Day 4 - Scratchcards/part 1/test_scratchcards.py
+++ Day 4 - Scratchcards/part 1/test_scratchcards.py
@@ -3,5 +3,13 @@ from scratchcards import Scratchcard
 
 def test_get_winning_and_i_have_numbers():
     card = Scratchcard("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53")
-    assert card.winning_numbers == (41, 48, 83, 86, 17)
-    assert card.numbers_i_have == (83, 86, 6, 31, 17, 9, 48, 53)
+    assert card.winning_numbers == {41, 48, 83, 86, 17}
+    assert card.numbers_i_have == {83, 86, 6, 31, 17, 9, 48, 53}
+
+
+def test_get_winning_numbers_i_have():
+    card = Scratchcard("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53")
+    assert card.winning_numbers_i_have == {48, 83, 17, 86}
+
+    card = Scratchcard("Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36")
+    assert card.winning_numbers_i_have == set()
