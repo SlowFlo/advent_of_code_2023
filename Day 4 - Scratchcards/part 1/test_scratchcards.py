3dfbf51063208a5712026b03be97adb1c1366fe9
SlowFlo <fbautry@gmail.com>
2023-12-10 17:03:49 +0100

    test: get winning and i have numbers

---
+++ Day 4 - Scratchcards/part 1/test_scratchcards.py
@@ -0,0 +1,18 @@
+def test_get_winning_and_i_have_numbers():
+    card = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
+    assert get_winning_and_i_have_numbers(card) == (
+        41,
+        48,
+        83,
+        86,
+        17,
+    ), (
+        83,
+        86,
+        6,
+        31,
+        17,
+        9,
+        48,
+        53,
+    )
