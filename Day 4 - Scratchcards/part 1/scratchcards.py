e9522be683a85fb1de5e8fe2728c0ca4ea02a969
SlowFlo <fbautry@gmail.com>
2023-12-10 17:58:57 +0100

    refactor: class version

--- Day 4 - Scratchcards/part 1/scratchcards.py
+++ Day 4 - Scratchcards/part 1/scratchcards.py
@@ -1,16 +1,13 @@
 import re
 
 
-def get_winning_and_i_have_numbers(
-    card: str,
-) -> tuple[tuple[int, ...], tuple[int, ...]]:
-    _, winning_numbers, i_have_numbers = re.split(r"[:|]", card)
+class Scratchcard:
+    def __init__(self, card: str):
+        _, winning_numbers, numbers_i_have = re.split(r"[:|]", card)
 
-    winning_numbers = tuple(
-        int(winning_number) for winning_number in winning_numbers.split()
-    )
-    i_have_numbers = tuple(
-        int(i_have_numbers) for i_have_numbers in i_have_numbers.split()
-    )
-
-    return winning_numbers, i_have_numbers
+        self.winning_numbers = tuple(
+            int(winning_number) for winning_number in winning_numbers.split()
+        )
+        self.numbers_i_have = tuple(
+            int(numbers_i_have) for numbers_i_have in numbers_i_have.split()
+        )
--- Day 4 - Scratchcards/part 1/test_scratchcards.py
+++ Day 4 - Scratchcards/part 1/test_scratchcards.py
@@ -1,9 +1,7 @@
-from scratchcards import get_winning_and_i_have_numbers
+from scratchcards import Scratchcard
 
 
 def test_get_winning_and_i_have_numbers():
-    card = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
-    assert get_winning_and_i_have_numbers(card) == (
-        (41, 48, 83, 86, 17),
-        (83, 86, 6, 31, 17, 9, 48, 53),
-    )
+    card = Scratchcard("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53")
+    assert card.winning_numbers == (41, 48, 83, 86, 17)
+    assert card.numbers_i_have == (83, 86, 6, 31, 17, 9, 48, 53)
