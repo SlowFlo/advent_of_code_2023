9e52a722a6928fbc529b34fa1385c075961627f3
SlowFlo <fbautry@gmail.com>
2023-12-12 14:01:09 +0100

    feat: convert when number in range

--- Day 5 - If You Give A Seed A Fertilizer/part 1/seed_and_fertilizer.py
+++ Day 5 - If You Give A Seed A Fertilizer/part 1/seed_and_fertilizer.py
@@ -1,2 +1,7 @@
 def convert(num_id: int, table: str) -> int:
+    for line in table.splitlines():
+        destination_start, source_start, range_length = map(int, line.split())
+        if num_id in range(source_start, source_start + range_length):
+            return destination_start + num_id - source_start
+
     return num_id
