ff2b25ef1a23999f23ecbcafa0bd9382742ef7c3
SlowFlo <fbautry@gmail.com>
2023-12-12 13:43:51 +0100

    test: convert when number in range

--- Day 5 - If You Give A Seed A Fertilizer/part 1/test_seed_and_fertilizer.py
+++ Day 5 - If You Give A Seed A Fertilizer/part 1/test_seed_and_fertilizer.py
@@ -1,5 +1,17 @@
+from seed_and_fertilizer import convert
+
+
 def test_convert_when_not_in_range():
     table = """50 98 2
 52 50 48"""
 
     assert convert(1, table) == 1
+    assert convert(30, table) == 30
+
+
+def test_convert_when_in_range():
+    table = """50 98 2
+52 50 48"""
+
+    assert convert(98, table) == 50
+    assert convert(61, table) == 63
